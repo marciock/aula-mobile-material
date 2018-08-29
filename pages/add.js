cdc7377613726123f928a2ae92258f295d8bfea2
marciock <marciockalves@gmail.com>
2018-08-29 06:04:27 -0300

    aula

---
+++ pages/add.js
@@ -0,0 +1,16 @@
+const add=`  
+      <div class="container">
+        <div class="row">
+          <form>
+            <div class="row">
+              <div class="input-field col s8">
+                <input type="text" id="nome">
+                <label for="nome">Nome</label>
+              </div>
+
+            </div>
+          </form>
+        </div>
+
+      </div>
+`
