cb5813509423efabca800b53af65eb00ad29136d
marciock <marciockalves@gmail.com>
2018-09-14 10:36:32 -0300

    v

--- pages/add.js
+++ pages/add.js
@@ -3,11 +3,15 @@ const add=`
         <div class="row">
           <form>
             <div class="row">
-              <div class="input-field col s8">
+              <div class="input-field col s12">
                 <input type="text" id="nome">
                 <label for="nome">Nome</label>
               </div>
+              
 
+            </div>
+            <div class="row">
+                <h3 id="resultado"></h3>
             </div>
           </form>
         </div>
